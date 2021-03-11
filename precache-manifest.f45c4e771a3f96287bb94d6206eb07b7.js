8ebce0cc7091fa758812d678c3b4fbe98cce6033
Pavnesh Chaturvedi <2019kuec2037@iiitkota.ac.in>
2021-03-11 19:41:29 +0530

    Updates

---
+++ precache-manifest.f45c4e771a3f96287bb94d6206eb07b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262508ddde19c027d1f38d0fcf1f5c00",
+    "url": "/pdf-viewer/index.html"
+  },
+  {
+    "revision": "f442512ffc8807274f19",
+    "url": "/pdf-viewer/static/css/main.707c5cf3.chunk.css"
+  },
+  {
+    "revision": "002c6d87f4fa2a1873ea",
+    "url": "/pdf-viewer/static/js/2.059b7808.chunk.js"
+  },
+  {
+    "revision": "f442512ffc8807274f19",
+    "url": "/pdf-viewer/static/js/main.602a5e0c.chunk.js"
+  },
+  {
+    "revision": "2a0e497f6c7c22c023e8",
+    "url": "/pdf-viewer/static/js/runtime~main.d7f9aec6.js"
+  },
+  {
+    "revision": "c7d354ff10c868752f1b032e5b0e55e8",
+    "url": "/pdf-viewer/static/media/sample.c7d354ff.pdf"
+  }
+]);
